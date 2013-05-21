75d9305bc119d87be01460d69c85724f8873c858
firebie <firebie@gmail.com>
2013-05-21 17:00:27 +0300

    init

---
+++ Editor/Editor.SmartHomeEnd.lua
@@ -0,0 +1,120 @@
+local function SelectBlock(info, sel, pos)
+  if not sel then
+    if pos > info.CurPos then
+      editor.Select(info.EditorId, 1, info.CurLine, info.CurPos, pos - info.CurPos, 1)
+    else
+      editor.Select(info.EditorId, 1, info.CurLine, pos, info.CurPos - pos, 1)  
+    end
+  else
+    if info.CurLine < sel.EndLine or (info.CurLine == sel.EndLine and info.CurPos < sel.EndPos) then
+      if info.CurLine == sel.EndLine and (pos - 1) == sel.EndPos then
+        editor.Select(info.EditorId, 0)
+      else
+        editor.Select(info.EditorId, 1, info.CurLine, pos, sel.EndPos - pos + 1, sel.EndLine - info.CurLine + 1)
+      end
+    else
+      editor.Select(info.EditorId, 1, sel.StartLine, sel.StartPos, pos - sel.StartPos, info.CurLine - sel.StartLine + 1)
+    end
+  end
+end
+
+local function SmartHome(select)
+  local info = editor.GetInfo()
+  local sel  = editor.GetSelection()
+    
+  if info then
+    local str = editor.GetString(-1, info.CurLine)
+    local s = str.StringText
+    local len = s:len()
+    local set = false 
+      
+    for i = 1, len do
+      local c = s:sub(i, i)
+      if c ~= " " and c ~= "\t" then
+        
+        local pos = i
+        if pos == info.CurPos then
+          pos = 1
+        end
+          
+        editor.SetPosition(info.EditorId, info.CurLine, pos)
+        if (select) then
+          SelectBlock(info, sel, pos)
+        elseif band(info.Options, far.Flags.EOPT_PERSISTENTBLOCKS) == 0 then
+          editor.Select(info.EditorId, 0)
+        end  
+
+        set = true
+        break
+      end
+    end
+ 
+    if not set then
+      editor.SetPosition(info.EditorId, info.CurLine, 1)
+    end
+    
+  end
+end
+
+local function SmartEnd(select)
+  local info = editor.GetInfo()
+  local sel  = editor.GetSelection()
+
+  if info then
+    local str = editor.GetString(-1, info.CurLine)
+    local s = str.StringText
+    local len = s:len()
+    local set = false
+      
+    for i = len, 1, -1 do
+      local c = s:sub(i, i)
+      if c ~= " " and c ~= "\t" then
+        local pos = i + 1
+        if pos == info.CurPos then
+          pos = string.len(s) + 1
+        end
+          
+        editor.SetPosition(info.EditorId, info.CurLine, pos)
+
+        if (select) then
+          SelectBlock(info, sel, pos)
+        elseif band(info.Options, far.Flags.EOPT_PERSISTENTBLOCKS) == 0 then
+          editor.Select(info.EditorId, 0)
+        end
+
+        set = true
+        break
+      end
+    end
+
+    if not set then
+      editor.SetPosition(info.EditorId, info.CurLine, len + 1)
+    end
+ end
+end
+
+
+Macro {
+  area="Editor"; key="Home"; flags=""; action = function()
+    SmartHome(false)
+  end;
+}
+
+
+Macro {
+  area="Editor"; key="ShiftHome"; flags=""; action = function()
+    SmartHome(true)
+  end;
+}
+
+Macro {
+  area="Editor"; key="End"; flags=""; action = function()
+    SmartEnd(false)
+  end;
+}
+
+Macro {
+  area="Editor"; key="ShiftEnd"; flags=""; action = function()
+    SmartEnd(true)
+  end;
+}
